16d12ab8881d5ffd5a310ec41c639687a8481b6f
f-teruhisa <teru_fukumoto@outlook.jp>
2021-02-11 14:08:18 +0900

    [Add] Installation Vue Router

---
+++ src/LoginButton.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="login-button" class="main-btn gradient-btn gradient-btn-2 wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">
+    <router-link to="/login">はじめる</router-link>
+    <router-view/>
+  </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import Vue from "vue";
+import LoginButton from "./LoginButton.vue";
 import "./assets/styles/index.css";
+import router from './router'
 
 Vue.config.productionTip = false;
 
-new Vue();
+new Vue({
+  router,
+  render: h => h(LoginButton)
+}).$mount("#login-button");
